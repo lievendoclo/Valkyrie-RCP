1fbaf33bc501a71a82ff78c82918f95fa07215ce
Lieven Doclo <lieven.doclo@jintec.be>
2011-04-08 11:38:00 +0200

    provide script to install 3rd party libs

---
+++ 3rd-party-libs/install.bat
@@ -0,0 +1,7 @@
+call mvn install:install-file -Dfile=l2fprod-common-all-7.3.jar -DpomFile=l2fprod-common-all-7.3.pom
+call mvn install:install-file -Dfile=vldocking-2.1.4.jar -DpomFile=vldocking-2.1.4.pom
+call mvn install:install-file -Dfile=TableLayout-20050920.jar -DpomFile=TableLayout-20050920.pom
+call mvn install:install-file -Dfile=mydoggy-1.5.0.pom -DpomFile=mydoggy-1.5.0.pom
+call mvn install:install-file -Dfile=mydoggy-api-1.5.0.jar -DpomFile=mydoggy-api-1.5.0.pom
+call mvn install:install-file -Dfile=mydoggy-res-1.5.0.jar -DpomFile=mydoggy-res-1.5.0.pom
+call mvn install:install-file -Dfile=mydoggy-plaf-1.5.0.jar -DpomFile=mydoggy-plaf-1.5.0.pom
