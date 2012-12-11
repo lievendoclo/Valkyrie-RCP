3c064f55a826fc366c537e0703c87472a3c0dbb0
Carsten Madsen <com@navicon.dk>
2012-12-11 12:29:33 +0100

    Support new'ing via @Configurable

--- valkyrie-rcp-core/src/main/java/org/valkyriercp/form/builder/CheckBoxFormComponentInterceptor.java
+++ valkyrie-rcp-core/src/main/java/org/valkyriercp/form/builder/CheckBoxFormComponentInterceptor.java
@@ -1,30 +1,41 @@
 package org.valkyriercp.form.builder;
 
+import java.util.Locale;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.valkyriercp.binding.form.FormModel;
 
-import javax.swing.*;
-import java.util.Locale;
-
 /**
- * <code>FormComponentInterceptor</code> that allows customization on how a CheckBox form property is rendered.
- *
- * @see CheckBoxFormComponentInterceptorFactory for more details on configuration
- *
+ * <code>FormComponentInterceptor</code> that allows customization on how a
+ * CheckBox form property is rendered.
+ * 
+ * @see CheckBoxFormComponentInterceptorFactory for more details on
+ *      configuration
+ * 
  * @author Peter De Bruycker
  */
-public class CheckBoxFormComponentInterceptor extends AbstractFormComponentInterceptor {
+@Configurable
+public class CheckBoxFormComponentInterceptor extends
+		AbstractFormComponentInterceptor {
 
 	private boolean showLabel = true;
 	private boolean showText = false;
 
 	private String textKey = "text";
 
+	@Autowired
 	private MessageSource messageSource;
 
-	public CheckBoxFormComponentInterceptor(FormModel formModel, MessageSource messageSource, boolean showLabel,
-			boolean showText, String textKey) {
+	public CheckBoxFormComponentInterceptor(FormModel formModel,
+			MessageSource messageSource, boolean showLabel, boolean showText,
+			String textKey) {
 		super(formModel);
 		this.messageSource = messageSource;
 		this.showLabel = showLabel;
@@ -42,30 +53,35 @@ public class CheckBoxFormComponentInterceptor extends AbstractFormComponentInter
 		}
 	}
 
-	protected String fetchExtraText(MessageSource messageSource, String propertyName) {
-		return messageSource.getMessage(new DefaultMessageSourceResolvable(getExtraTextKeys(propertyName), propertyName
-				+ "." + textKey), Locale.getDefault());
+	protected String fetchExtraText(MessageSource messageSource,
+			String propertyName) {
+		return messageSource.getMessage(new DefaultMessageSourceResolvable(
+				getExtraTextKeys(propertyName), propertyName + "." + textKey),
+				Locale.getDefault());
 	}
 
 	/**
-	 * Returns the keys used to fetch the extra text from the <code>MessageSource</code>.
+	 * Returns the keys used to fetch the extra text from the
+	 * <code>MessageSource</code>.
 	 * <p>
 	 * The keys returned are
 	 * <code>&lt;formModelId&gt;.&lt;propertyName&gt;.&lt;textKey&gt;, &lt;propertyName&gt;.&lt;textKey&gt;, &lt;textKey&gt;</code>
 	 * <p>
 	 * Can safely be overridden to add extra keys
-	 *
+	 * 
 	 * @param propertyName
 	 *            the property name
 	 * @return the keys
 	 */
 	protected String[] getExtraTextKeys(String propertyName) {
-		return new String[] { getFormModel().getId() + "." + propertyName + "." + textKey,
+		return new String[] {
+				getFormModel().getId() + "." + propertyName + "." + textKey,
 				propertyName + "." + textKey, textKey };
 	}
 
 	protected boolean isCheckBoxProperty(String propertyName) {
-		Class type = getFormModel().getFieldMetadata(propertyName).getPropertyType();
+		Class type = getFormModel().getFieldMetadata(propertyName)
+				.getPropertyType();
 
 		return Boolean.TYPE.equals(type) || Boolean.class.equals(type);
 	}
@@ -76,4 +92,3 @@ public class CheckBoxFormComponentInterceptor extends AbstractFormComponentInter
 		}
 	}
 }
-
