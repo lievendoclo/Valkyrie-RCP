c62a3de5452a6025158b3d15e56109f4d8e6c919
Carsten Madsen <com@navicon.dk>
2013-10-30 11:12:42 +0100

    Merging. Switched to vldocking 3.0.0

--- valkyrie-rcp-integrations/valkyrie-rcp-vldocking/src/main/java/org/valkyriercp/application/docking/ViewDescriptorDockable.java
+++ valkyrie-rcp-integrations/valkyrie-rcp-vldocking/src/main/java/org/valkyriercp/application/docking/ViewDescriptorDockable.java
@@ -18,89 +18,89 @@ package org.valkyriercp.application.docking;
 import java.awt.Component;
 
 import org.springframework.core.style.ToStringCreator;
+import org.valkyriercp.application.PageComponent;
+import org.valkyriercp.application.ViewDescriptor;
 
 import com.vlsolutions.swing.docking.DockKey;
 import com.vlsolutions.swing.docking.Dockable;
 import com.vlsolutions.swing.docking.DockingConstants.Hide;
-import org.valkyriercp.application.PageComponent;
-import org.valkyriercp.application.ViewDescriptor;
 
 /**
  * @author Rogan Dawes
  */
 public class ViewDescriptorDockable implements Dockable {
 
-    private ViewDescriptor viewDescriptor;
+	private ViewDescriptor viewDescriptor;
 
-    private PageComponent pageComponent;
+	private PageComponent pageComponent;
 
-    private DockKey dockKey;
+	private DockKey dockKey;
 
-    public ViewDescriptorDockable(ViewDescriptor viewDescriptor) {
-        this(viewDescriptor, null);
-    }
+	public ViewDescriptorDockable(ViewDescriptor viewDescriptor) {
+		this(viewDescriptor, null);
+	}
 
-    public ViewDescriptorDockable(ViewDescriptor viewDescriptor,
-            PageComponent pageComponent) {
-        this.viewDescriptor = viewDescriptor;
-        this.pageComponent = pageComponent;
-        this.dockKey = new DockKey(viewDescriptor.getId());
+	public ViewDescriptorDockable(ViewDescriptor viewDescriptor,
+			PageComponent pageComponent) {
+		this.viewDescriptor = viewDescriptor;
+		this.pageComponent = pageComponent;
+		this.dockKey = new DockKey(viewDescriptor.getId());
 
-        dockKey.setName(viewDescriptor.getDisplayName());
-        dockKey.setTooltip(viewDescriptor.getCaption());
-        dockKey.setIcon(viewDescriptor.getIcon());
+		dockKey.setName(viewDescriptor.getDisplayName());
+		dockKey.setTooltip(viewDescriptor.getCaption());
+		dockKey.setIcon(viewDescriptor.getIcon());
 
-        boolean autoHideEnabled = VLDockingViewDescriptor.DEFAULT_AUTOHIDEENABLED;
-        Hide autoHideBorder = VLDockingViewDescriptor.DEFAULT_AUTOHIDEBORDER;
-        boolean closeEnabled = VLDockingViewDescriptor.DEFAULT_CLOSEENABLED;
-        boolean floatEnabled = VLDockingViewDescriptor.DEFAULT_FLOATENABLED;
-        boolean maximizeEnabled = VLDockingViewDescriptor.DEFAULT_MAXIMIZEENABLED;
+		boolean autoHideEnabled = VLDockingViewDescriptor.DEFAULT_AUTOHIDEENABLED;
+		Hide autoHideBorder = VLDockingViewDescriptor.DEFAULT_AUTOHIDEBORDER;
+		boolean closeEnabled = VLDockingViewDescriptor.DEFAULT_CLOSEENABLED;
+		boolean floatEnabled = VLDockingViewDescriptor.DEFAULT_FLOATENABLED;
+		boolean maximizeEnabled = VLDockingViewDescriptor.DEFAULT_MAXIMIZEENABLED;
 
-        if (viewDescriptor instanceof VLDockingViewDescriptor) {
-            VLDockingViewDescriptor dockingViewDescriptor = (VLDockingViewDescriptor) viewDescriptor;
-            autoHideEnabled = dockingViewDescriptor.isAutoHideEnabled();
-            autoHideBorder = dockingViewDescriptor.getAutoHideBorder();
-            closeEnabled = dockingViewDescriptor.isCloseEnabled();
-            floatEnabled = dockingViewDescriptor.isFloatEnabled();
-            maximizeEnabled = dockingViewDescriptor.isMaximizeEnabled();
-        }
-        dockKey.setAutoHideEnabled(autoHideEnabled);
-        dockKey.setAutoHideBorder(autoHideBorder);
-        dockKey.setCloseEnabled(closeEnabled);
-        dockKey.setFloatEnabled(floatEnabled);
-        dockKey.setMaximizeEnabled(maximizeEnabled);
-    }
+		if (viewDescriptor instanceof VLDockingViewDescriptor) {
+			VLDockingViewDescriptor dockingViewDescriptor = (VLDockingViewDescriptor) viewDescriptor;
+			autoHideEnabled = dockingViewDescriptor.isAutoHideEnabled();
+			autoHideBorder = dockingViewDescriptor.getAutoHideBorder();
+			closeEnabled = dockingViewDescriptor.isCloseEnabled();
+			floatEnabled = dockingViewDescriptor.isFloatEnabled();
+			maximizeEnabled = dockingViewDescriptor.isMaximizeEnabled();
+		}
+		dockKey.setAutoHideEnabled(autoHideEnabled);
+		dockKey.setAutoHideBorder(autoHideBorder);
+		dockKey.setCloseEnabled(closeEnabled);
+		dockKey.setFloatEnabled(floatEnabled);
+		dockKey.setMaximizeEnabled(maximizeEnabled);
+	}
 
-    public PageComponent getPageComponent() {
-        if (pageComponent == null)
-            pageComponent = viewDescriptor.createPageComponent();
-        return pageComponent;
-    }
+	public PageComponent getPageComponent() {
+		if (pageComponent == null)
+			pageComponent = viewDescriptor.createPageComponent();
+		return pageComponent;
+	}
 
-    public void setPageComponent(PageComponent pageComponent) {
-        this.pageComponent = pageComponent;
-    }
+	public void setPageComponent(PageComponent pageComponent) {
+		this.pageComponent = pageComponent;
+	}
 
-    /*
-    * (non-Javadoc)
-    *
-    * @see com.vlsolutions.swing.docking.Dockable#getComponent()
-    */
-    public Component getComponent() {
-        return getPageComponent().getControl();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vlsolutions.swing.docking.Dockable#getComponent()
+	 */
+	public Component getComponent() {
+		return getPageComponent().getControl();
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see com.vlsolutions.swing.docking.Dockable#getDockKey()
-     */
-    public DockKey getDockKey() {
-        return dockKey;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vlsolutions.swing.docking.Dockable#getDockKey()
+	 */
+	public DockKey getDockKey() {
+		return dockKey;
+	}
 
-    public String toString() {
-        return new ToStringCreator(this).append("viewDescriptor",
-                viewDescriptor.getId()).toString();
-    }
+	public String toString() {
+		return new ToStringCreator(this).append("viewDescriptor",
+				viewDescriptor.getId()).toString();
+	}
 }
