674d3b90338f844f0fbb227b91b357d2b7e09919
Lieven Doclo <lievendoclo1983@gmail.com>
2015-03-13 13:46:02 +0100

    set correct jdk for travis

---
+++ valkyrie-rcp-core/src/test/groovy/org/valkyriercp/AssertJTest.groovy
@@ -0,0 +1,11 @@
+package org.valkyriercp
+
+import spock.lang.Specification
+
+
+/**
+ * Created by lievendoclo on 28/01/15.
+ */
+class AssertJTest extends Specification {
+
+}
---
+++ valkyrie-rcp-samples/valkyrie-rcp-showcase-sample/src/test/java/BinderDemoWidgetSpec.groovy
@@ -0,0 +1,9 @@
+import spock.lang.Specification
+
+
+/**
+ * Created by lievendoclo on 28/01/15.
+ */
+class BinderDemoWidgetSpec extends Specification {
+
+}
